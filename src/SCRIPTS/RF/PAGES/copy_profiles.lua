5a4e149589862564ad98a951ebae3ba6082ee07e
egonl <34315684+egonl@users.noreply.github.com>
2021-12-01 12:47:19 +0100

    Added Copy Profiles page. (#3)

---
+++ src/SCRIPTS/RF/PAGES/copy_profiles.lua
@@ -0,0 +1,44 @@
+local template = assert(loadScript(radio.template))()
+local margin = template.margin
+local indent = template.indent
+local lineSpacing = template.lineSpacing
+local tableSpacing = template.tableSpacing
+local sp = template.listSpacing.field
+local yMinLim = radio.yMinLimit
+local x = margin
+local y = yMinLim - lineSpacing
+local inc = { x = function(val) x = x + val return x end, y = function(val) y = y + val return y end }
+local labels = {}
+local fields = {}
+
+fields[#fields + 1] = { t = "Profile type",                x = x, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 1, vals = { 1 }, table = { [0] = "PID", "Rate" } }
+fields[#fields + 1] = { t = "Source profile",              x = x, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 5, vals = { 3 }, table = { [0] = "1", "2", "3", "4", "5", "6" } }
+fields[#fields + 1] = { t = "Dest. profile",               x = x, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 5, vals = { 2 }, table = { [0] = "1", "2", "3", "4", "5", "6" } }
+labels[#labels + 1] = { t = "Use Save to copy the source", x = x, y = inc.y(lineSpacing) }
+labels[#labels + 1] = { t = "profile to the destination.", x = x, y = inc.y(lineSpacing) }
+
+return {
+    read        = 101, -- MSP_STATUS
+    write       = 183, -- MSP_COPY_PROFILE
+    reboot      = false,
+    eepromWrite = true,
+    title       = "Copy",
+    minBytes    = 24,
+    labels      = labels,
+    fields      = fields,
+    postRead = function(self)
+        self.maxPidProfiles = self.values[15]
+        self.currentPidProfile = self.values[16]
+        self.values = { 0, self.getDestinationPidProfile(self), self.currentPidProfile }
+        self.minBytes = 3
+    end,
+    getDestinationPidProfile = function(self)
+        local destPidProfile
+        if (self.currentPidProfile < self.maxPidProfiles - 1) then
+            destPidProfile = self.currentPidProfile + 1
+        else
+            destPidProfile = self.currentPidProfile - 1
+        end
+        return destPidProfile
+    end, 
+}
--- src/SCRIPTS/RF/pages.lua
+++ src/SCRIPTS/RF/pages.lua
@@ -4,6 +4,7 @@ local PageFiles = {}
 PageFiles[#PageFiles + 1] = { title = "PIDs", script = "pids.lua" }
 PageFiles[#PageFiles + 1] = { title = "Profile", script = "profile.lua" }
 PageFiles[#PageFiles + 1] = { title = "Rates", script = "ratesrf.lua" }
+PageFiles[#PageFiles + 1] = { title = "Copy Profiles", script = "copy_profiles.lua" }
 PageFiles[#PageFiles + 1] = { title = "Governor", script = "governor.lua" }
 PageFiles[#PageFiles + 1] = { title = "Filters", script = "filters.lua" }
 PageFiles[#PageFiles + 1] = { title = "Receiver", script = "rxrf.lua" }
