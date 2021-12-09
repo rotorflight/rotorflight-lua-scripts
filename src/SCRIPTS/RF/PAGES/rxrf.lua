f5c13d756337f82a896e490941e33a8574c1f78d
egonl <34315684+egonl@users.noreply.github.com>
2021-12-09 10:28:32 +0100

    rxrf.lua refactored for MSP_RX_CONFIG message (#5)

--- src/SCRIPTS/RF/PAGES/rxrf.lua
+++ src/SCRIPTS/RF/PAGES/rxrf.lua
@@ -22,37 +22,24 @@ local channels = { [0] =
     "YTC", "RYTC", "PYTC", "RPYTC",
 }
 
-if apiVersion >= 1.016 then
-    labels[#labels + 1] = { t = "Stick",  x = x,          y = inc.y(lineSpacing) }
-    fields[#fields + 1] = { t = "Low",    x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 1000, max = 2000, vals = { 6, 7 } }
-    fields[#fields + 1] = { t = "Center", x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 1000, max = 2000, vals = { 4, 5 } }
-    fields[#fields + 1] = { t = "High",   x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 1000, max = 2000, vals = { 2, 3 } }
-end
+labels[#labels + 1] = { t = "Stick",             x = x,          y = inc.y(lineSpacing) }
+fields[#fields + 1] = { t = "Low",               x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 1000, max = 2000, vals = { 8, 9 } }
+fields[#fields + 1] = { t = "Center",            x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 1000, max = 2000, vals = { 6, 7 } }
+fields[#fields + 1] = { t = "High",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 1000, max = 2000, vals = { 4, 5 } }
 
-if apiVersion >= 1.040 then
-    labels[#labels + 1] = { t = "RC Smoothing", x = x,          y = inc.y(lineSpacing) }
-    fields[#fields + 1] = { t = "Type",         x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 1, vals = { 25 }, table = { [0] = "Interpolation", "Filter" } }
-    fields[#fields + 1] = { t = "Channels",     x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = #channels, vals = { 24 }, table = channels }
-    labels[#labels + 1] = { t = "Input Filter",      x = x,          y = inc.y(lineSpacing) }
-    fields[#fields + 1] = { t = "Cutoff",            x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 255, vals = { 26 }, table = { [0] = "Auto" } }
-    fields[#fields + 1] = { t = "Type",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 1, vals = { 28 }, table = { [0] = "PT1", "BIQUAD"} }
-    labels[#labels + 1] = { t = "Derivative Filter", x = x,          y = inc.y(lineSpacing) }
-    fields[#fields + 1] = { t = "Cutoff",            x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 255, vals = { 27 }, table = { [0] = "Auto" } }
-    fields[#fields + 1] = { t = "Type",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 3, vals = { 29 }, table = { [0] = "Off", "PT1", "BIQUAD", "Auto"} }
-end
+labels[#labels + 1] = { t = "RC Smoothing",      x = x,          y = inc.y(lineSpacing) }
+fields[#fields + 1] = { t = "Type",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 1, vals = { 17 }, table = { [0] = "Interpolation", "Filter" } }
+fields[#fields + 1] = { t = "Channels",          x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = #channels, vals = { 16 }, table = channels }
+labels[#labels + 1] = { t = "Input Filter",      x = x,          y = inc.y(lineSpacing) }
+fields[#fields + 1] = { t = "Cutoff",            x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 255, vals = { 19 }, table = { [0] = "Auto" } }
+fields[#fields + 1] = { t = "Type",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 1, vals = { 18 }, table = { [0] = "PT1", "BIQUAD"} }
+labels[#labels + 1] = { t = "Derivative Filter", x = x,          y = inc.y(lineSpacing) }
+fields[#fields + 1] = { t = "Cutoff",            x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 255, vals = { 21 }, table = { [0] = "Auto" } }
+fields[#fields + 1] = { t = "Type",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 3, vals = { 20 }, table = { [0] = "Off", "PT1", "BIQUAD", "Auto"} }
+fields[#fields + 1] = { t = "Auto Smoothness",   x = x, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 50, vals = { 22 } }
 
-if apiVersion >= 1.020 then
-    fields[#fields + 1] = { t = "Interpolation", x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 3, vals = { 13 }, table={ [0]="Off", "Preset", "Auto", "Manual"} }
-    fields[#fields + 1] = { t = "Interval",      x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 1, max = 50, vals = { 14 } }
-end
-
-if apiVersion >= 1.042 then
-    fields[#fields + 1] = { t = "Auto Smoothness", x = x, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 50, vals = { 31 } }
-end
-
---if apiVersion >= 1.031 then
---    fields[#fields + 1] = { t = "Cam Angle", x = x, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 90, vals = { 23 } }
---end
+fields[#fields + 1] = { t = "Interpolation",     x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 3, vals = { 14 }, table={ [0]="Off", "Preset", "Auto", "Manual"} }
+fields[#fields + 1] = { t = "Interval",          x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 1, max = 50, vals = { 15 } }
 
 return {
     read        = 44, -- MSP_RX_CONFIG
@@ -60,7 +47,7 @@ return {
     title       = "Receiver",
     reboot      = false,
     eepromWrite = true,
-    minBytes    = 12,
+    minBytes    = 28,
     labels      = labels,
     fields      = fields,
 }
