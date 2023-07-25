09e06901124af6c66d72e63adcaf516756bbecc9
egonl <34315684+egonl@users.noreply.github.com>
2023-07-26 01:30:13 +0200

    chore: aligned with firmware 230724 (#27)

--- src/SCRIPTS/RF2/PAGES/profile_rescue.lua
+++ src/SCRIPTS/RF2/PAGES/profile_rescue.lua
@@ -22,13 +22,14 @@ fields[#fields + 1] = { t = "Flip fail time",        x = x,          y = inc.y(l
 fields[#fields + 1] = { t = "Exit time",             x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 250,   vals = { 8 }, scale = 10 }
 fields[#fields + 1] = { t = "Rescue level gain",     x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 5, max = 250,   vals = { 4 } }
 fields[#fields + 1] = { t = "Rescue flip gain",      x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 5, max = 250,   vals = { 3 } }
-fields[#fields + 1] = { t = "Rescue max rate",       x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 1, max = 1000,  vals = { 23,24 }, mult = 10 }
-fields[#fields + 1] = { t = "Rescue max accel",      x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 1, max = 10000, vals = { 25,26 }, mult = 10 }
+fields[#fields + 1] = { t = "Rescue max rate",       x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 1, max = 1000,  vals = { 25,26 }, mult = 10 }
+fields[#fields + 1] = { t = "Rescue max accel",      x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 1, max = 10000, vals = { 27,28 }, mult = 10 }
 labels[#labels + 1] = { t = "Altitude hold",         x = x,          y = inc.y(lineSpacing) }
-fields[#fields + 1] = { t = "Hover altitude",        x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 10000, vals = { 15,16 }, mult = 10 }
+fields[#fields + 1] = { t = "Hover altitude",        x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 10000, vals = { 15,16 }, mult = 10, scale = 100 }
 fields[#fields + 1] = { t = "P-gain",                x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 10000, vals = { 17,18 }, mult = 10 }
 fields[#fields + 1] = { t = "I-gain",                x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 10000, vals = { 19,20 }, mult = 10 }
-fields[#fields + 1] = { t = "Max collective",        x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 1, max = 1000,  vals = { 21,22 }, mult = 10 }
+fields[#fields + 1] = { t = "D-gain",                x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 10000, vals = { 21,22 }, mult = 10 }
+fields[#fields + 1] = { t = "Max collective",        x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 1, max = 1000,  vals = { 23,24 }, mult = 10, scale = 10 }
 
 return {
     read        = 146, -- MSP_RESCUE_PROFILE
@@ -36,7 +37,7 @@ return {
     title       = "Profile - Rescue",
     reboot      = false,
     eepromWrite = true,
-    minBytes    = 26,
+    minBytes    = 28,
     labels      = labels,
     fields      = fields,
 }
