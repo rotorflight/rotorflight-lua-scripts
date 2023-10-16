4ff26082932f0b7406a8e554891905dc7c7c4365
egonl <egon.lubbers@gmail.com>
2023-10-16 23:49:35 +0200

    Added adjustment teller

--- src/SCRIPTS/FUNCTIONS/rf2bg.lua
+++ src/SCRIPTS/FUNCTIONS/rf2bg.lua
@@ -1,8 +1,8 @@
-chdir("/SCRIPTS/RF2")
+rfBaseDir = "/SCRIPTS/RF2/"
 apiVersion = 0
-protocol = assert(loadScript("protocols.lua"))()
-assert(loadScript(protocol.mspTransport))()
-assert(loadScript("MSP/common.lua"))()
-local background = assert(loadScript("background.lua"))()
+protocol = assert(loadScript(rfBaseDir.."protocols.lua"))()
+assert(loadScript(rfBaseDir..protocol.mspTransport))()
+assert(loadScript(rfBaseDir.."MSP/common.lua"))()
+local background = assert(loadScript(rfBaseDir.."background.lua"))()
 
 return { run=background }
--- src/SCRIPTS/RF2/background.lua
+++ src/SCRIPTS/RF2/background.lua
@@ -1,45 +1,48 @@
 local apiVersionReceived = false
 local timeIsSet = false
-local getApiVersion, setRtc, rssiTask
-local rssiEnabled = true
+local getApiVersion, setRtc, adjTellerTask
+local adjTellerEnabled = true
 
 local function run_bg()
     if getRSSI() > 0 then
         -- Send data when the telemetry connection is available
         -- assuming when sensor value higher than 0 there is an telemetry connection
         if not apiVersionReceived then
-            getApiVersion = getApiVersion or assert(loadScript("api_version.lua"))()
+            getApiVersion = getApiVersion or assert(loadScript(rfBaseDir.."api_version.lua"))()
             apiVersionReceived = getApiVersion.f()
             if apiVersionReceived then
                 getApiVersion = nil
                 collectgarbage()
             end
         elseif not timeIsSet then
-            setRtc = setRtc or assert(loadScript("rtc.lua"))()
+            setRtc = setRtc or assert(loadScript(rfBaseDir.."rtc.lua"))()
             timeIsSet = setRtc.f()
             if timeIsSet then
                 setRtc = nil
                 collectgarbage()
             end
-        elseif rssiEnabled and apiVersion >= 1.37 then
-            rssiTask = rssiTask or assert(loadScript("rssi.lua"))()
-            rssiEnabled = rssiTask()
-            if not rssiEnabled then
-                rssiTask = nil
+        elseif adjTellerEnabled and protocol.mspTransport == "MSP/sp.lua" then
+            adjTellerTask = adjTellerTask or assert(loadScript(rfBaseDir.."adj_teller.lua"))()
+            adjTellerEnabled = adjTellerTask.run()
+            if adjTellerEnabled == 2 then
+                adjTellerTask = nil
                 collectgarbage()
+                return 2
             end
         end
     else
         apiVersionReceived = false
         timeIsSet = false
-        rssiEnabled = true
-        if getApiVersion or setRtc or rssiTask then
+        adjTellerEnabled = true
+        if getApiVersion or setRtc or adjTellerTask then
             getApiVersion = nil
             setRtc = nil
-            rssiTask = nil
+            adjTellerTask = nil
             collectgarbage()
         end
     end
+
+    return 0
 end
 
 return run_bg
