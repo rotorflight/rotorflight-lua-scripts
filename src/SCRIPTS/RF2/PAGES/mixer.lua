df005a72db33f91710e7fac73617ab1eaefb3aa5
egonl <egon.lubbers@gmail.com>
2024-01-03 23:40:31 +0100

    Aligned with firmware 240103

--- src/SCRIPTS/RF2/PAGES/mixer.lua
+++ src/SCRIPTS/RF2/PAGES/mixer.lua
@@ -12,19 +12,20 @@ local labels = {}
 local fields = {}
 
 labels[#labels + 1] = { t = "Swashplate",               x = x,          y = inc.y(lineSpacing) }
-fields[#fields + 1] = { t = "Total pitch limit",        x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0,     max = 3000, vals = { 9, 10 }, scale = 83.33333333333333, mult = 8.3333333333333 }
-fields[#fields + 1] = { t = "Phase angle",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -1800, max = 1800, vals = { 7, 8 }, scale = 10, mult = 5 }
-fields[#fields + 1] = { t = "Cyclic ring",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0,     max = 100, vals = { 6 } }
-fields[#fields + 1] = { t = "Col. TTA precomp",         x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0,     max = 250, vals = { 14 } }
+fields[#fields + 1] = { t = "Geo correction",           x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -125,  max = 125,  vals = { 19 }, scale = 5 }
+fields[#fields + 1] = { t = "Total pitch limit",        x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0,     max = 3000, vals = { 10, 11 }, scale = 83.33333333333333, mult = 8.3333333333333 }
+fields[#fields + 1] = { t = "Phase angle",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -1800, max = 1800, vals = { 8, 9 }, scale = 10, mult = 5 }
+fields[#fields + 1] = { t = "Cyclic ring",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0,     max = 100,  vals = { 7 } }
+fields[#fields + 1] = { t = "TTA precomp",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0,     max = 250,  vals = { 18 } }
 
 labels[#labels + 1] = { t = "Swashplate link trims",    x = x,          y = inc.y(lineSpacing) }
-fields[#fields + 1] = { t = "Link trim #1",             x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -128, max = 127, vals = { 11 } }
-fields[#fields + 1] = { t = "Link trim #2",             x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -128, max = 127, vals = { 12 } }
-fields[#fields + 1] = { t = "Link trim #3",             x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -128, max = 127, vals = { 13 } }
+fields[#fields + 1] = { t = "Link trim #1",             x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -1000, max = 1000, vals = { 12, 13 }, scale = 10 }
+fields[#fields + 1] = { t = "Link trim #2",             x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -1000, max = 1000, vals = { 14, 15 }, scale = 10 }
+fields[#fields + 1] = { t = "Link trim #3",             x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -1000, max = 1000, vals = { 16, 17 }, scale = 10 }
 
 labels[#labels + 1] = { t = "Motorised tail",           x = x,          y = inc.y(lineSpacing) }
-fields[#fields + 1] = { t = "Motor idle thr%",          x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0,    max = 250, vals = { 3 }, scale = 10 }
-fields[#fields + 1] = { t = "Center trim",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -125, max = 125, vals = { 4 }, scale = 4 }
+fields[#fields + 1] = { t = "Motor idle thr%",          x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0,     max = 250,  vals = { 3 }, scale = 10 }
+fields[#fields + 1] = { t = "Center trim",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -500,  max = 500,  vals = { 4,5 }, scale = 10 }
 
 return {
     read        = 42, -- MSP_MIXER_CONFIG
@@ -32,7 +33,7 @@ return {
     eepromWrite = true,
     reboot      = false,
     title       = "Mixer",
-    minBytes    = 14,
+    minBytes    = 19,
     labels      = labels,
     fields      = fields,
 }
