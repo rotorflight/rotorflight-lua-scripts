52630fdc47fee8e6dea59cc4f1584d92db994e42
egonl <egon.lubbers@gmail.com>
2024-01-10 11:59:44 +0100

    Renamed link trims

--- src/SCRIPTS/RF2/PAGES/mixer.lua
+++ src/SCRIPTS/RF2/PAGES/mixer.lua
@@ -19,9 +19,9 @@ fields[#fields + 1] = { t = "Cyclic ring",              x = x + indent, y = inc.
 fields[#fields + 1] = { t = "TTA precomp",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0,     max = 250,  vals = { 18 } }
 
 labels[#labels + 1] = { t = "Swashplate link trims",    x = x,          y = inc.y(lineSpacing) }
-fields[#fields + 1] = { t = "Link trim #1",             x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -1000, max = 1000, vals = { 12, 13 }, scale = 10 }
-fields[#fields + 1] = { t = "Link trim #2",             x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -1000, max = 1000, vals = { 14, 15 }, scale = 10 }
-fields[#fields + 1] = { t = "Link trim #3",             x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -1000, max = 1000, vals = { 16, 17 }, scale = 10 }
+fields[#fields + 1] = { t = "Roll trim %",              x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -1000, max = 1000, vals = { 12, 13 }, scale = 10 }
+fields[#fields + 1] = { t = "Pitch trim %",             x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -1000, max = 1000, vals = { 14, 15 }, scale = 10 }
+fields[#fields + 1] = { t = "Coll. trim %",             x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -1000, max = 1000, vals = { 16, 17 }, scale = 10 }
 
 labels[#labels + 1] = { t = "Motorised tail",           x = x,          y = inc.y(lineSpacing) }
 fields[#fields + 1] = { t = "Motor idle thr%",          x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0,     max = 250,  vals = { 3 }, scale = 10 }
