045f70d2a5b638ca3833b7c1d0fa5cad4ec1ddda
egonl <34315684+egonl@users.noreply.github.com>
2024-03-28 17:30:07 +0100

    Removed altitude hold

--- src/SCRIPTS/RF2/PAGES/profile_rescue.lua
+++ src/SCRIPTS/RF2/PAGES/profile_rescue.lua
@@ -11,7 +11,8 @@ local inc = { x = function(val) x = x + val return x end, y = function(val) y = 
 local labels = {}
 local fields = {}
 
-fields[#fields + 1] = { t = "Rescue mode enable",    x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 2,     vals = { 1 }, table = { [0] = "Off", "On", "Alt hold" } }
+--fields[#fields + 1] = { t = "Rescue mode enable",    x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 2,     vals = { 1 }, table = { [0] = "Off", "On", "Alt hold" } }
+fields[#fields + 1] = { t = "Rescue mode enable",    x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 1,     vals = { 1 }, table = { [0] = "Off", "On" } }
 fields[#fields + 1] = { t = "Flip to upright",       x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 1,     vals = { 2 }, table = { [0] = "No flip", "Flip" } }
 fields[#fields + 1] = { t = "Pull-up collective",    x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 1000,  vals = { 9,10 }, mult = 10, scale = 10 }
 fields[#fields + 1] = { t = "Pull-up time",          x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 250,   vals = { 5 }, scale = 10 }
@@ -24,12 +25,14 @@ fields[#fields + 1] = { t = "Rescue level gain",     x = x,          y = inc.y(l
 fields[#fields + 1] = { t = "Rescue flip gain",      x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 5, max = 250,   vals = { 3 } }
 fields[#fields + 1] = { t = "Rescue max rate",       x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 1, max = 1000,  vals = { 25,26 }, mult = 10 }
 fields[#fields + 1] = { t = "Rescue max accel",      x = x,          y = inc.y(lineSpacing), sp = x + sp, min = 1, max = 10000, vals = { 27,28 }, mult = 10 }
+--[[
 labels[#labels + 1] = { t = "Altitude hold",         x = x,          y = inc.y(lineSpacing) }
 fields[#fields + 1] = { t = "Hover altitude",        x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 10000, vals = { 15,16 }, mult = 10, scale = 100 }
 fields[#fields + 1] = { t = "P-gain",                x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 10000, vals = { 17,18 } }
 fields[#fields + 1] = { t = "I-gain",                x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 10000, vals = { 19,20 } }
 fields[#fields + 1] = { t = "D-gain",                x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 10000, vals = { 21,22 } }
 fields[#fields + 1] = { t = "Max collective",        x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 1, max = 1000,  vals = { 23,24 }, mult = 10, scale = 10 }
+--]]
 
 return {
     read        = 146, -- MSP_RESCUE_PROFILE
