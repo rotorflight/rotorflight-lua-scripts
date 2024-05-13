5b4739e83f4bf30cfdeed1cf73edf42cee5ca2da
shmuely <offer.shmuely@gmail.com>
2024-05-13 17:04:40 +0300

    move rates to top of menu

--- src/SCRIPTS/RF2/pages.lua
+++ src/SCRIPTS/RF2/pages.lua
@@ -1,12 +1,12 @@
 local PageFiles = {}
 
 -- Rotorflight pages.
+PageFiles[#PageFiles + 1] = { title = "Rates", script = "rates.lua" }
 PageFiles[#PageFiles + 1] = { title = "Profile - PIDs", script = "pids.lua" }
 PageFiles[#PageFiles + 1] = { title = "Profile - Governor", script = "profile_governor.lua" }
 PageFiles[#PageFiles + 1] = { title = "Profile - Rescue", script = "profile_rescue.lua" }
 PageFiles[#PageFiles + 1] = { title = "Profile - Various", script = "profile.lua" }
 PageFiles[#PageFiles + 1] = { title = "Copy profiles", script = "copy_profiles.lua" }
-PageFiles[#PageFiles + 1] = { title = "Rates", script = "rates.lua" }
 PageFiles[#PageFiles + 1] = { title = "Governor", script = "governor.lua" }
 PageFiles[#PageFiles + 1] = { title = "Servos", script = "servos.lua" }
 PageFiles[#PageFiles + 1] = { title = "Mixer", script = "mixer.lua" }
