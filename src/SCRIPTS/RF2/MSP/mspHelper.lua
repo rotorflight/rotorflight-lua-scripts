cc795810af6c6a479ab04cb50fb1f6ddcc716241
egonl <egon.lubbers@gmail.com>
2024-06-22 23:16:59 +0200

    Using bit32 in mspHelper

--- src/SCRIPTS/RF2/MSP/mspHelper.lua
+++ src/SCRIPTS/RF2/MSP/mspHelper.lua
@@ -8,15 +8,18 @@ local mspHelper = {
     readU16 = function(buf)
         local offset = buf.offset or 1
         local value = buf[offset]
-        value = value | buf[offset + 1] << 8
+        --value = value | buf[offset + 1] << 8
+        value = bit32.bor(value, bit32.lshift(buf[offset + 1], 8))
         buf.offset = offset + 2
         return value
     end,
     readS16 = function(buf)
         local offset = buf.offset or 1
         local value = buf[offset]
-        value = value | buf[offset + 1] << 8
-        if value & (1 << 15) ~= 0 then value = value - (2 ^ 16) end
+        --value = value | buf[offset + 1] << 8
+        value = bit32.bor(value, bit32.lshift(buf[offset + 1], 8))
+        --if value & (1 << 15) ~= 0 then value = value - (2 ^ 16) end
+        if bit32.band(value, bit32.lshift(1, 15)) ~= 0 then value = value - (2 ^ 16) end
         buf.offset = offset + 2
         return value
     end,
@@ -24,26 +27,34 @@ local mspHelper = {
         local offset = buf.offset or 1
         local value = 0
         for i = 0, 3 do
-            value = value | buf[offset + i] << (i * 8)
+            --value = value | buf[offset + i] << (i * 8)
+            value = bit32.bor(value, bit32.lshift(buf[offset + i], 8))
         end
         buf.offset = offset + 2
         return value
     end,
     writeU8 = function(buf, value)
-        local byte1 = value & 0xFF
+        --local byte1 = value & 0xFF
+        local byte1 = bit32.band(value,  0xFF)
         table.insert(buf, byte1)
     end,
     writeU16 = function(buf, value)
-        local byte1 = value & 0xFF
-        local byte2 = (value >> 8) & 0xFF
+        --local byte1 = value & 0xFF
+        local byte1 = bit32.band(value,  0xFF)
+        --local byte2 = (value >> 8) & 0xFF
+        local byte2 = bit32.band(bit32.rshift(value, 8), 0xFF)
         table.insert(buf, byte1)
         table.insert(buf, byte2)
     end,
     writeU32 = function(buf, value)
-        local byte1 = value & 0xFF
-        local byte2 = (value >> 8) & 0xFF
-        local byte3 = (value >> 16) & 0xFF
-        local byte4 = (value >> 24) & 0xFF
+        --local byte1 = value & 0xFF
+        local byte1 = bit32.band(value,  0xFF)
+        --local byte2 = (value >> 8) & 0xFF
+        local byte2 = bit32.band(bit32.rshift(value, 8), 0xFF)
+        --local byte3 = (value >> 16) & 0xFF
+        local byte3 = bit32.band(bit32.rshift(value, 16), 0xFF)
+        --local byte4 = (value >> 24) & 0xFF
+        local byte4 = bit32.band(bit32.rshift(value, 24), 0xFF)
         table.insert(buf, byte1)
         table.insert(buf, byte2)
         table.insert(buf, byte3)
