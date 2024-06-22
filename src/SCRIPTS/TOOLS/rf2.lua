0c421c4a9c67e966a6b420c503d11d09ff545b57
egonl <34315684+egonl@users.noreply.github.com>
2024-06-22 11:54:46 +0200

    chore: moved code in tools to rf2 (#46)
    * move all code into directory for more reliability
    
    * Renamed main.lua to tool.lua
    
    ---------
    
    Co-authored-by: shmuely <offer.shmuely@gmail.com>

--- src/SCRIPTS/TOOLS/rf2.lua
+++ src/SCRIPTS/TOOLS/rf2.lua
@@ -1,21 +1,8 @@
 local toolName = "TNS|Rotorflight 2|TNE"
-chdir("/SCRIPTS/RF2")
 
-apiVersion = 0
-mcuId = nil
-runningInSimulator = string.sub(select(2,getVersion()), -4) == "simu"
-
-local run = nil
-local scriptsCompiled = assert(loadScript("COMPILE/scripts_compiled.lua"))()
-
-if scriptsCompiled then
-    protocol = assert(loadScript("protocols.lua"))()
-    radio = assert(loadScript("radios.lua"))().msp
-    assert(loadScript(protocol.mspTransport))()
-    assert(loadScript("MSP/common.lua"))()
-    run = assert(loadScript("ui.lua"))()
-else
-    run = assert(loadScript("COMPILE/compile.lua"))()
+local function run()
+    return "/SCRIPTS/RF2/tool.lua"
 end
 
-return { run=run }
+return { run = run }
+
