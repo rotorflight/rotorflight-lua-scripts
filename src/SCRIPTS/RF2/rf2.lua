144d800aab7afa47a2c8be0a34881fe7388a0f35
egonl <egon.lubbers@gmail.com>
2024-06-26 18:38:34 +0200

    Slightly better formatting

--- src/SCRIPTS/RF2/rf2.lua
+++ src/SCRIPTS/RF2/rf2.lua
@@ -1,12 +1,15 @@
 rf2 = {
     baseDir = "/SCRIPTS/RF2/",
     runningInSimulator = string.sub(select(2,getVersion()), -4) == "simu",
+
     loadScript = loadScript,
+
     log = function(str)
         local f = io.open("/LOGS/rf2.log", 'a')
         io.write(f, tostring(str) .. "\n")
         io.close(f)
     end,
+
     print = function(str)
         if rf2.runningInSimulator then
             print(tostring(str))
@@ -15,6 +18,7 @@ rf2 = {
             --rf2.log(str)
         end
     end,
+
     clock = function()
         return getTime() / 100
     end
