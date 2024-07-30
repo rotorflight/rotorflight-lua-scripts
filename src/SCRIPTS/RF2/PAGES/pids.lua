a6ad991ad873609b4c015bc131ebbb7b75859693
egonl <egon.lubbers@gmail.com>
2024-07-30 23:57:08 +0200

    Added Current PID Profile to PIDs page

--- src/SCRIPTS/RF2/PAGES/pids.lua
+++ src/SCRIPTS/RF2/PAGES/pids.lua
@@ -1,4 +1,6 @@
 local template = assert(rf2.loadScript(rf2.radio.template))()
+local mspSetProfile = assert(rf2.loadScript("MSP/mspSetProfile.lua"))()
+local mspStatus = assert(rf2.loadScript("MSP/mspStatus.lua"))()
 local margin = template.margin
 local indent = template.indent
 local lineSpacing = template.lineSpacing
@@ -11,50 +13,61 @@ local y = yMinLim - lineSpacing
 local inc = { x = function(val) x = x + val return x end, y = function(val) y = y + val return y end }
 local labels = {}
 local fields = {}
+local editing = false
+
+local startEditing = function(field, page)
+    editing = true
+end
+
+local endPidEditing = function(field, page)
+    mspSetProfile.setPidProfile(field.data.value, function() rf2.reloadPage() end, nil)
+end
+
+fields[#fields + 1] = { t = "Current PID profile", x = x, y = inc.y(lineSpacing), sp = x + sp * 1.17, data = { value = nil, min = 0, max = 5, table = { [0] = "1", "2", "3", "4", "5", "6" } }, preEdit = startEditing, postEdit = endPidEditing }
 
 x = margin
-y = yMinLim - tableSpacing.header
+y = yMinLim + lineSpacing * 0.2
 labels[#labels + 1] = { t = "",      x = x, y = inc.y(tableSpacing.header) }
 labels[#labels + 1] = { t = "Ro",     x = x, y = inc.y(tableSpacing.row) }
 labels[#labels + 1] = { t = "Pi",     x = x, y = inc.y(tableSpacing.row) }
 labels[#labels + 1] = { t = "Ya",     x = x, y = inc.y(tableSpacing.row) }
 
 x = x + tableSpacing.col/2
-y = yMinLim - tableSpacing.header
+y = yMinLim + lineSpacing * 0.25
 labels[#labels + 1] = { t = "P",     x = x, y = inc.y(tableSpacing.header) }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 1,2 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 9,10 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 17,18 } }
 
 x = x + colSpacing
-y = yMinLim - tableSpacing.header
+y = yMinLim + lineSpacing * 0.25
 labels[#labels + 1] = { t = "I",     x = x, y = inc.y(tableSpacing.header) }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 3,4 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 11,12 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 19,20 } }
 
 x = x + colSpacing
-y = yMinLim - tableSpacing.header
+y = yMinLim + lineSpacing * 0.25
 labels[#labels + 1] = { t = "O",     x = x, y = inc.y(tableSpacing.header) }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 31,32 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 33,34 } }
 
 x = x + colSpacing
-y = yMinLim - tableSpacing.header
+y = yMinLim + lineSpacing * 0.25
 labels[#labels + 1] = { t = "D",     x = x, y = inc.y(tableSpacing.header) }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 5,6 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 13,14 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 21,22 } }
 
 x = x + colSpacing
-y = yMinLim - tableSpacing.header
+y = yMinLim + lineSpacing * 0.25
 labels[#labels + 1] = { t = "F",     x = x, y = inc.y(tableSpacing.header) }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 7,8 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 15,16 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 23,24 } }
 
 x = x + colSpacing
-y = yMinLim - tableSpacing.header
+y = yMinLim + lineSpacing * 0.25
 labels[#labels + 1] = { t = "B",     x = x, y = inc.y(tableSpacing.header) }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 25,26 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 27,28 } }
@@ -70,4 +83,20 @@ return {
     labels      = labels,
     fields      = fields,
     simulatorResponse = {70, 0, 225, 0, 90, 0, 120, 0, 100, 0, 200, 0, 70, 0, 120, 0, 100, 0, 125, 0, 83, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 0, 25, 0 },
+    postLoad = function(self)
+        mspStatus.getStatus(self.onProcessedMspStatus, self)
+    end,
+--[[
+    timer = function(self)
+        if rf2.mspQueue:isProcessed() then
+            if not editing then
+                mspStatus.getStatus(self.onProcessedMspStatus, self)
+            end
+        end
+    end,
+--]]
+    onProcessedMspStatus = function(self, status)
+        fields[1].data.value = status.profile
+        self.isReady = true
+    end,
 }
