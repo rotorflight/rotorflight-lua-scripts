6b2c0d96f296554bfe2995758cbf3cc18a6808e5
egonl <egon.lubbers@gmail.com>
2024-08-02 22:49:20 +0200

    Changed MSP API versions for testing

--- src/SCRIPTS/RF2/MSP/mspApiVersion.lua
+++ src/SCRIPTS/RF2/MSP/mspApiVersion.lua
@@ -7,7 +7,7 @@ local function getApiVersion(callback, callbackParam)
                 callback(callbackParam, version)
             end
         end,
-        simulatorResponse = { 0, 12, 6 }
+        simulatorResponse = { 0, 12, 7 }
     }
     rf2.mspQueue:add(message)
 end
--- src/SCRIPTS/RF2/pages.lua
+++ src/SCRIPTS/RF2/pages.lua
@@ -13,7 +13,7 @@ PageFiles[#PageFiles + 1] = { title = "Mixer", script = "mixer.lua" }
 PageFiles[#PageFiles + 1] = { title = "Filters", script = "filters.lua" }
 PageFiles[#PageFiles + 1] = { title = "Accelerometer trim", script = "accelerometer.lua" }
 
-if rf2.apiVersion >= 12.07 then
+if rf2.apiVersion >= 12.06 then  -- TODO: set to 12.07
     PageFiles[#PageFiles + 1] = { title = "ESC - HW Platinum V5", script = "esc_hwpl5.lua" }
     PageFiles[#PageFiles + 1] = { title = "ESC - Scorpion Tribunus", script = "esc_scorp.lua" }
     PageFiles[#PageFiles + 1] = { title = "ESC - YGE", script = "esc_yge.lua" }
