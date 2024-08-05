115b069e219b69da1a595db85786640c3de46e48
egonl <egon.lubbers@gmail.com>
2024-08-05 22:47:23 +0200

    Added profile copy for PIDs page

--- src/SCRIPTS/RF2/PAGES/pids.lua
+++ src/SCRIPTS/RF2/PAGES/pids.lua
@@ -16,6 +16,55 @@ local fields = {}
 local editing = false
 local profileAdjustmentTS = nil
 
+x = margin
+local tableStartY = yMinLim - lineSpacing
+y = tableStartY
+labels[#labels + 1] = { t = "",      x = x, y = inc.y(tableSpacing.header) }
+labels[#labels + 1] = { t = "Ro",     x = x, y = inc.y(tableSpacing.row) }
+labels[#labels + 1] = { t = "Pi",     x = x, y = inc.y(tableSpacing.row) }
+labels[#labels + 1] = { t = "Ya",     x = x, y = inc.y(tableSpacing.row) }
+
+x = x + tableSpacing.col/2
+y = tableStartY
+labels[#labels + 1] = { t = "P",     x = x, y = inc.y(tableSpacing.header) }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 1,2 } }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 9,10 } }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 17,18 } }
+
+x = x + colSpacing
+y = tableStartY
+labels[#labels + 1] = { t = "I",     x = x, y = inc.y(tableSpacing.header) }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 3,4 } }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 11,12 } }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 19,20 } }
+
+x = x + colSpacing
+y = tableStartY
+labels[#labels + 1] = { t = "O",     x = x, y = inc.y(tableSpacing.header) }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 31,32 } }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 33,34 } }
+
+x = x + colSpacing
+y = tableStartY
+labels[#labels + 1] = { t = "D",     x = x, y = inc.y(tableSpacing.header) }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 5,6 } }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 13,14 } }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 21,22 } }
+
+x = x + colSpacing
+y = tableStartY
+labels[#labels + 1] = { t = "F",     x = x, y = inc.y(tableSpacing.header) }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 7,8 } }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 15,16 } }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 23,24 } }
+
+x = x + colSpacing
+y = tableStartY
+labels[#labels + 1] = { t = "B",     x = x, y = inc.y(tableSpacing.header) }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 25,26 } }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 27,28 } }
+fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 29,30 } }
+
 local startEditing = function(field, page)
     editing = true
 end
@@ -24,55 +73,25 @@ local endPidEditing = function(field, page)
     mspSetProfile.setPidProfile(field.data.value, function() rf2.reloadPage() end, nil)
 end
 
-fields[#fields + 1] = { t = "Current PID profile", x = x, y = inc.y(lineSpacing), sp = x + sp * 1.17, data = { value = nil, min = 0, max = 5, table = { [0] = "1", "2", "3", "4", "5", "6" } }, preEdit = startEditing, postEdit = endPidEditing }
-
 x = margin
-y = yMinLim + lineSpacing * 0.2
-labels[#labels + 1] = { t = "",      x = x, y = inc.y(tableSpacing.header) }
-labels[#labels + 1] = { t = "Ro",     x = x, y = inc.y(tableSpacing.row) }
-labels[#labels + 1] = { t = "Pi",     x = x, y = inc.y(tableSpacing.row) }
-labels[#labels + 1] = { t = "Ya",     x = x, y = inc.y(tableSpacing.row) }
+fields[18] = { t = "Current PID profile",             x = x,          y = inc.y(lineSpacing * 1.3), sp = x + sp * 1.17, data = { value = nil, min = 0, max = 5, table = { [0] = "1", "2", "3", "4", "5", "6" } }, preEdit = startEditing, postEdit = endPidEditing }
+fields[19] = { t = "Destination profile",             x = x,          y = inc.y(lineSpacing),       sp = x + sp * 1.17, data = { value = nil, min = 0, max = 5, table = { [0] = "1", "2", "3", "4", "5", "6" } } }
 
-x = x + tableSpacing.col/2
-y = yMinLim + lineSpacing * 0.25
-labels[#labels + 1] = { t = "P",     x = x, y = inc.y(tableSpacing.header) }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 1,2 } }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 9,10 } }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 17,18 } }
+local function copyProfile(field, page)
+    local source = page.fields[18].data.value
+    local dest = page.fields[19].data.value
+    if source == dest then return end
 
-x = x + colSpacing
-y = yMinLim + lineSpacing * 0.25
-labels[#labels + 1] = { t = "I",     x = x, y = inc.y(tableSpacing.header) }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 3,4 } }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 11,12 } }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 19,20 } }
+    local mspCopyProfile = {
+        command = 183, -- MSP_COPY_PROFILE
+        payload = { 0, dest, source }
+    }
 
-x = x + colSpacing
-y = yMinLim + lineSpacing * 0.25
-labels[#labels + 1] = { t = "O",     x = x, y = inc.y(tableSpacing.header) }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 31,32 } }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 33,34 } }
+    rf2.mspQueue:add(mspCopyProfile)
+    rf2.settingsSaved()
+end
 
-x = x + colSpacing
-y = yMinLim + lineSpacing * 0.25
-labels[#labels + 1] = { t = "D",     x = x, y = inc.y(tableSpacing.header) }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 5,6 } }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 13,14 } }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 21,22 } }
-
-x = x + colSpacing
-y = yMinLim + lineSpacing * 0.25
-labels[#labels + 1] = { t = "F",     x = x, y = inc.y(tableSpacing.header) }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 7,8 } }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 15,16 } }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 23,24 } }
-
-x = x + colSpacing
-y = yMinLim + lineSpacing * 0.25
-labels[#labels + 1] = { t = "B",     x = x, y = inc.y(tableSpacing.header) }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 25,26 } }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 27,28 } }
-fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 29,30 } }
+fields[#fields + 1] = { t = "[Copy Current to Dest]", x = x + indent, y = inc.y(lineSpacing), preEdit = copyProfile }
 
 return {
     read        = 112, -- MSP_PID_TUNING
@@ -84,9 +103,11 @@ return {
     labels      = labels,
     fields      = fields,
     simulatorResponse = {70, 0, 225, 0, 90, 0, 120, 0, 100, 0, 200, 0, 70, 0, 120, 0, 100, 0, 125, 0, 83, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 0, 25, 0 },
+
     postLoad = function(self)
         mspStatus.getStatus(self.onProcessedMspStatus, self)
     end,
+
     timer = function(self)
         if profileAdjustmentTS and rf2.clock() - profileAdjustmentTS > 0.5 then
             rf2.reloadPage()
@@ -94,13 +115,25 @@ return {
             mspStatus.getStatus(self.onProcessedMspStatus, self)
         end
     end,
+
     onProcessedMspStatus = function(self, status)
-        if fields[1].data.value ~= status.profile and not editing then
-            if fields[1].data.value then
+        local currentField = self.fields[18]
+        if currentField.data.value ~= status.profile and not editing then
+            if currentField.data.value then
                 profileAdjustmentTS = rf2.clock()
             end
-            fields[1].data.value = status.profile
+            currentField.data.value = status.profile
         end
+
+        local destField = self.fields[19]
+        if not destField.data.value then
+            if status.profile < 5 then
+                destField.data.value = status.profile + 1
+            else
+                destField.data.value = 4
+            end
+        end
+
         self.isReady = true
     end,
 }
