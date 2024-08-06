0bf881bcffbcbe718e5e96afc5898b60213e509f
egonl <34315684+egonl@users.noreply.github.com>
2024-08-06 23:01:49 +0200

    feat: added 'Copy profile' to PIDs and Rates pages (#61)

--- src/SCRIPTS/RF2/PAGES/pids.lua
+++ src/SCRIPTS/RF2/PAGES/pids.lua
@@ -24,55 +24,74 @@ local endPidEditing = function(field, page)
     mspSetProfile.setPidProfile(field.data.value, function() rf2.reloadPage() end, nil)
 end
 
-fields[#fields + 1] = { t = "Current PID profile", x = x, y = inc.y(lineSpacing), sp = x + sp * 1.17, data = { value = nil, min = 0, max = 5, table = { [0] = "1", "2", "3", "4", "5", "6" } }, preEdit = startEditing, postEdit = endPidEditing }
+local function copyProfile(field, page)
+    local source = page.fields[18].data.value
+    local dest = page.fields[19].data.value
+    if source == dest then return end
+
+    local mspCopyProfile = {
+        command = 183, -- MSP_COPY_PROFILE
+        payload = { 0, dest, source } -- 0 = copy pids
+    }
+
+    rf2.mspQueue:add(mspCopyProfile)
+    rf2.settingsSaved()
+end
 
 x = margin
-y = yMinLim + lineSpacing * 0.2
+local tableStartY = yMinLim - lineSpacing
+y = tableStartY
 labels[#labels + 1] = { t = "",      x = x, y = inc.y(tableSpacing.header) }
 labels[#labels + 1] = { t = "Ro",     x = x, y = inc.y(tableSpacing.row) }
 labels[#labels + 1] = { t = "Pi",     x = x, y = inc.y(tableSpacing.row) }
 labels[#labels + 1] = { t = "Ya",     x = x, y = inc.y(tableSpacing.row) }
 
 x = x + tableSpacing.col/2
-y = yMinLim + lineSpacing * 0.25
+y = tableStartY
 labels[#labels + 1] = { t = "P",     x = x, y = inc.y(tableSpacing.header) }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 1,2 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 9,10 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 17,18 } }
 
 x = x + colSpacing
-y = yMinLim + lineSpacing * 0.25
+y = tableStartY
 labels[#labels + 1] = { t = "I",     x = x, y = inc.y(tableSpacing.header) }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 3,4 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 11,12 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 19,20 } }
 
 x = x + colSpacing
-y = yMinLim + lineSpacing * 0.25
+y = tableStartY
 labels[#labels + 1] = { t = "O",     x = x, y = inc.y(tableSpacing.header) }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 31,32 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 33,34 } }
 
 x = x + colSpacing
-y = yMinLim + lineSpacing * 0.25
+y = tableStartY
 labels[#labels + 1] = { t = "D",     x = x, y = inc.y(tableSpacing.header) }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 5,6 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 13,14 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 21,22 } }
 
 x = x + colSpacing
-y = yMinLim + lineSpacing * 0.25
+y = tableStartY
 labels[#labels + 1] = { t = "F",     x = x, y = inc.y(tableSpacing.header) }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 7,8 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 15,16 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 23,24 } }
 
 x = x + colSpacing
-y = yMinLim + lineSpacing * 0.25
+y = tableStartY
 labels[#labels + 1] = { t = "B",     x = x, y = inc.y(tableSpacing.header) }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 25,26 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 27,28 } }
 fields[#fields + 1] = {              x = x, y = inc.y(tableSpacing.row), min = 0, max = 1000, vals = { 29,30 } }
+
+x = margin
+inc.y(lineSpacing * 0.25)
+fields[18] = { t = "Current PID profile",             x = x,          y = inc.y(lineSpacing), sp = x + sp * 1.17, data = { value = nil, min = 0, max = 5, table = { [0] = "1", "2", "3", "4", "5", "6" } }, preEdit = startEditing, postEdit = endPidEditing }
+fields[19] = { t = "Destination profile",             x = x,          y = inc.y(lineSpacing), sp = x + sp * 1.17, data = { value = nil, min = 0, max = 5, table = { [0] = "1", "2", "3", "4", "5", "6" } } }
+fields[#fields + 1] = { t = "[Copy Current to Dest]", x = x + indent, y = inc.y(lineSpacing), preEdit = copyProfile }
 
 return {
     read        = 112, -- MSP_PID_TUNING
@@ -84,23 +103,37 @@ return {
     labels      = labels,
     fields      = fields,
     simulatorResponse = {70, 0, 225, 0, 90, 0, 120, 0, 100, 0, 200, 0, 70, 0, 120, 0, 100, 0, 125, 0, 83, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 0, 25, 0 },
+
     postLoad = function(self)
         mspStatus.getStatus(self.onProcessedMspStatus, self)
     end,
+
     timer = function(self)
-        if profileAdjustmentTS and rf2.clock() - profileAdjustmentTS > 0.5 then
+        if profileAdjustmentTS and rf2.clock() - profileAdjustmentTS > 0.35 then
             rf2.reloadPage()
         elseif rf2.mspQueue:isProcessed() and not editing then
             mspStatus.getStatus(self.onProcessedMspStatus, self)
         end
     end,
+
     onProcessedMspStatus = function(self, status)
-        if fields[1].data.value ~= status.profile and not editing then
-            if fields[1].data.value then
+        local currentField = self.fields[18]
+        if currentField.data.value ~= status.profile and not editing then
+            if currentField.data.value then
                 profileAdjustmentTS = rf2.clock()
             end
-            fields[1].data.value = status.profile
+            currentField.data.value = status.profile
         end
+
+        local destField = self.fields[19]
+        if not destField.data.value then
+            if status.profile < 5 then
+                destField.data.value = status.profile + 1
+            else
+                destField.data.value = 4
+            end
+        end
+
         self.isReady = true
     end,
 }
--- src/SCRIPTS/RF2/pages.lua
+++ src/SCRIPTS/RF2/pages.lua
@@ -6,12 +6,12 @@ PageFiles[#PageFiles + 1] = { title = "Profile - PIDs", script = "pids.lua" }
 PageFiles[#PageFiles + 1] = { title = "Profile - Governor", script = "profile_governor.lua" }
 PageFiles[#PageFiles + 1] = { title = "Profile - Rescue", script = "profile_rescue.lua" }
 PageFiles[#PageFiles + 1] = { title = "Profile - Various", script = "profile.lua" }
-PageFiles[#PageFiles + 1] = { title = "Copy profiles", script = "copy_profiles.lua" }
 PageFiles[#PageFiles + 1] = { title = "Rates", script = "rates.lua" }
 PageFiles[#PageFiles + 1] = { title = "Governor", script = "governor.lua" }
 PageFiles[#PageFiles + 1] = { title = "Servos", script = "servos.lua" }
 PageFiles[#PageFiles + 1] = { title = "Mixer", script = "mixer.lua" }
 PageFiles[#PageFiles + 1] = { title = "Filters", script = "filters.lua" }
 PageFiles[#PageFiles + 1] = { title = "Accelerometer trim", script = "accelerometer.lua" }
+PageFiles[#PageFiles + 1] = { title = "Copy profiles", script = "copy_profiles.lua" }
 
 return PageFiles
