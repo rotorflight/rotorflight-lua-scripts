a417cb10f2c8fb294a0eecfe62648ea8c22c77a3
egonl <egon.lubbers@gmail.com>
2024-08-25 23:40:34 +0200

    rf2bg now uses mspQueue

--- src/SCRIPTS/RF2/background.lua
+++ src/SCRIPTS/RF2/background.lua
@@ -1,32 +1,40 @@
 local timeIsSet = rf2.runningInSimulator
-local setRtc, adjTellerTask
+local mspSetRtc, adjTellerTask
 local adjTellerEnabled = true
 
+local function onRtcSet()
+    timeIsSet = true
+    playTone(1600, 500, 0, PLAY_BACKGROUND)
+    mspSetRtc = nil
+    collectgarbage()
+end
+
 local function run_bg()
-    if getRSSI() > 0 or rf2.runningInSimulator then
-        if not timeIsSet then
-            setRtc = setRtc or assert(rf2.loadScript(rf2.baseDir.."rtc.lua"))()
-            timeIsSet = setRtc.f()
-            if timeIsSet then
-                setRtc = nil
-                collectgarbage()
-            end
-        elseif adjTellerEnabled then
-            adjTellerTask = adjTellerTask or assert(rf2.loadScript(rf2.baseDir.."adj_teller.lua"))()
-            adjTellerEnabled = adjTellerTask.run()
-            if adjTellerEnabled == 2 then
-                adjTellerTask = nil
-                collectgarbage()
-                return 2
-            end
-        end
-    else
+    if getRSSI() == 0 and not rf2.runningInSimulator then
         timeIsSet = false
         adjTellerEnabled = true
-        if setRtc or adjTellerTask then
-            setRtc = nil
+        if mspSetRtc or adjTellerTask then
+            mspSetRtc = nil
             adjTellerTask = nil
             collectgarbage()
+        end
+        return
+    end
+
+    if not rf2.runningInSimulator and not timeIsSet and rf2.mspQueue:isProcessed() then
+        mspSetRtc = mspSetRtc or assert(rf2.loadScript(rf2.baseDir.."MSP/mspSetRtc.lua"))()
+        mspSetRtc.setRtc(onRtcSet)
+    end
+
+    rf2.mspQueue:processQueue()
+
+    if timeIsSet and adjTellerEnabled then
+        adjTellerTask = adjTellerTask or assert(rf2.loadScript(rf2.baseDir.."adj_teller.lua"))()
+        adjTellerEnabled = adjTellerTask.run()
+        if adjTellerEnabled == 2 then
+            adjTellerTask = nil
+            collectgarbage()
+            return 2
         end
     end
 
