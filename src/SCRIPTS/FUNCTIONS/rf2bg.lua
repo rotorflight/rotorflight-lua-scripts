8843c1abc8f018fe18aa57770109e25807593b5b
egonl <34315684+egonl@users.noreply.github.com>
2024-08-26 13:06:58 +0200

    rf2bg now uses mspQueue (#63)
    * rf2bg now uses mspQueue
    
    * Improved callback

--- src/SCRIPTS/FUNCTIONS/rf2bg.lua
+++ src/SCRIPTS/FUNCTIONS/rf2bg.lua
@@ -1,5 +1,8 @@
 assert(loadScript("/SCRIPTS/RF2/rf2.lua"))()
 rf2.protocol = assert(rf2.loadScript(rf2.baseDir.."protocols.lua"))()
+rf2.mspQueue = assert(rf2.loadScript(rf2.baseDir.."MSP/mspQueue.lua"))()
+rf2.mspQueue.maxRetries = rf2.protocol.maxRetries
+rf2.mspHelper = assert(rf2.loadScript(rf2.baseDir.."MSP/mspHelper.lua"))()
 assert(rf2.loadScript(rf2.baseDir..rf2.protocol.mspTransport))()
 assert(rf2.loadScript(rf2.baseDir.."MSP/common.lua"))()
 local background = assert(rf2.loadScript(rf2.baseDir.."background.lua"))()
---
+++ src/SCRIPTS/RF2/MSP/mspSetRtc.lua
@@ -0,0 +1,26 @@
+local function setRtc(callback, callbackParam)
+    local message = {
+        command = 246, -- MSP_SET_RTC
+        payload = {},
+        processReply = function(self, buf)
+            --rf2.print("RTC set.")
+            if callback then callback(callbackParam) end
+        end,
+        simulatorResponse = {}
+    }
+
+    local now = getRtcTime()
+    -- format: seconds after the epoch (32) / milliseconds (16)
+    for i = 1, 4 do
+        rf2.mspHelper.writeU8(message.payload, bit32.band(now, 0xFF))
+        now = bit32.rshift(now, 8)
+    end
+    -- we don't have milliseconds
+    rf2.mspHelper.writeU16(message.payload, 0)
+
+    rf2.mspQueue:add(message)
+end
+
+return {
+    setRtc = setRtc
+}
