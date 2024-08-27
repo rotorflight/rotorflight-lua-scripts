f1d982fe236086d725bd07ffad7246e8e79195c3
egonl <egon.lubbers@gmail.com>
2024-08-27 13:00:25 +0200

    Fixed build and compilation

--- src/SCRIPTS/RF2/COMPILE/compile.lua
+++ src/SCRIPTS/RF2/COMPILE/compile.lua
@@ -1,16 +1,19 @@
+-- ui.lua is quite big and compiling it later might throw an 'out of memory' error
+assert(loadScript("ui.lua", 'c'))
+
 local i = 1
 local scripts = assert(loadScript("COMPILE/scripts.lua"))
-
 collectgarbage()
 
 local function compile()
     local script = scripts(i)
     i = i + 1
     if script then
+        if script == "/SCRIPTS/RF2/ui.lua" then return 0 end
         lcd.clear()
         lcd.drawText(2, 2, "Compiling...", SMLSIZE)
         lcd.drawText(2, 22, script, SMLSIZE)
-        assert(loadScript("/SCRIPTS/"..script, 'c'))
+        assert(loadScript(script, 'c'))
         collectgarbage()
         return 0
     end
