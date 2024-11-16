f4d1a2fe9408b6ecd369f6ff552d14b4df257d31
rburrow87 <rob@rebrw.com>
2024-11-15 17:59:48 -0800

    Add gov_min_throttle (#74)

--- src/SCRIPTS/RF2/PAGES/profile_governor.lua
+++ src/SCRIPTS/RF2/PAGES/profile_governor.lua
@@ -17,6 +17,9 @@ fields[#fields + 1] = { t = "Current PID profile",     x = x,          y = inc.y
 inc.y(lineSpacing * 0.25)
 fields[#fields + 1] = { t = "Full headspeed",          x = x, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 50000, vals = { 1, 2 }, mult = 10, id = "govHeadspeed"}
 fields[#fields + 1] = { t = "Max throttle",            x = x, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 100, vals = { 13 }, id = "govMaxThrottle" }
+if rf2.apiVersion >= 12.07 then
+    fields[#fields + 1] = { t = "Min throttle",        x = x, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 100, vals = { 14 }, id = "govMinThrottle" }
+end
 fields[#fields + 1] = { t = "PID master gain",         x = x, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 250, vals = { 3 },  id = "govMasterGain" }
 fields[#fields + 1] = { t = "P-gain",                  x = x, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 250, vals = { 4 },  id = "govPGain" }
 fields[#fields + 1] = { t = "I-gain",                  x = x, y = inc.y(lineSpacing), sp = x + sp, min = 0, max = 250, vals = { 5 },  id = "govIGain" }
@@ -37,7 +40,7 @@ return {
     minBytes    = 13,
     labels      = labels,
     fields      = fields,
-    simulatorResponse = { 208, 7, 100, 10, 125, 5, 20, 0, 20, 10, 40, 100, 100 },
+    simulatorResponse = { 208, 7, 100, 10, 125, 5, 20, 0, 20, 10, 40, 100, 100, 10 },
     profileSwitcher = profileSwitcher,
 
     postLoad = function(self)
