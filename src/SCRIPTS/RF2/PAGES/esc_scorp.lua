3e49b9e9218ee2b1d0ede2445e374d63df3fcb8e
egonl <egon.lubbers@gmail.com>
2025-02-24 22:56:03 +0100

    fix: added power cycle message for Scorpion (#78)

--- src/SCRIPTS/RF2/PAGES/esc_scorp.lua
+++ src/SCRIPTS/RF2/PAGES/esc_scorp.lua
@@ -48,8 +48,8 @@ local onOff = {
 }
 
 labels[1] = { t = "ESC not ready, waiting...", x = x,      y = inc.y(lineSpacing) }
-labels[2] = { t = "---",                   x = x + indent, y = inc.y(lineSpacing), bold = false }
-labels[3] = { t = "---",                   x = x + indent, y = inc.y(lineSpacing), bold = false }
+labels[2] = { t = "  Please power cycle",      x = x + indent, y = inc.y(lineSpacing), bold = false }
+labels[3] = { t = "     your ESC now.",        x = x + indent, y = inc.y(lineSpacing), bold = false }
 
 --- Basic
 fields[1] = { t = "Flight Mode",           x = x,          y = inc.y(lineSpacing * 2), sp = x + sp, min = 0, max = #flightMode, vals = { 2+33, 2+34 }, table = flightMode }
