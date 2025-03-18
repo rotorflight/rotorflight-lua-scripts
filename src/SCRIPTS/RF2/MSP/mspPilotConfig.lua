2cf3f2c518e0c5683950dbcc61c9847a4ffc5320
egonl <egon.lubbers@gmail.com>
2025-03-18 20:52:36 +0100

    Refactored model.lua

--- src/SCRIPTS/RF2/MSP/mspPilotConfig.lua
+++ src/SCRIPTS/RF2/MSP/mspPilotConfig.lua
@@ -1,17 +1,15 @@
-local function getPilotConfig(callback, callbackParam)
+local function getPilotConfig(data, callback, callbackParam)
     local message = {
         command = 12, -- MSP_PILOT_CONFIG
         processReply = function(self, buf)
-            local config = {}
-            --rf2.print("buf length: "..#buf)
-            config.model_id = { value = rf2.mspHelper.readU8(buf), min = 0, max = 99 }
-            config.model_param1_type = { value = rf2.mspHelper.readU8(buf), min = 0, max = 12, table = { [0] = "NONE", "TIMER1", "TIMER2", "TIMER3", "GV1", "GV2", "GV3", "GV4", "GV5", "GV6", "GV7", "GV8", "GV9" } }
-            config.model_param1_value = { value = rf2.mspHelper.readS16(buf), min = -32000, max = 32000 }
-            config.model_param2_type = { value = rf2.mspHelper.readU8(buf), min = 0, max = 12, table = { [0] = "NONE", "TIMER1", "TIMER2", "TIMER3", "GV1", "GV2", "GV3", "GV4", "GV5", "GV6", "GV7", "GV8", "GV9" } }
-            config.model_param2_value = { value = rf2.mspHelper.readS16(buf), min = -32000, max = 32000 }
-            config.model_param3_type = { value = rf2.mspHelper.readU8(buf), min = 0, max = 12, table = { [0] = "NONE", "TIMER1", "TIMER2", "TIMER3", "GV1", "GV2", "GV3", "GV4", "GV5", "GV6", "GV7", "GV8", "GV9" } }
-            config.model_param3_value = { value = rf2.mspHelper.readS16(buf), min = -32000, max = 32000 }
-            callback(callbackParam, config)
+            data.model_id.value = rf2.mspHelper.readU8(buf)
+            data.model_param1_type.value = rf2.mspHelper.readU8(buf)
+            data.model_param1_value.value = rf2.mspHelper.readS16(buf)
+            data.model_param2_type.value = rf2.mspHelper.readU8(buf)
+            data.model_param2_value.value = rf2.mspHelper.readS16(buf)
+            data.model_param3_type.value = rf2.mspHelper.readU8(buf)
+            data.model_param3_value.value = rf2.mspHelper.readS16(buf)
+            callback(callbackParam)
         end,
         simulatorResponse = { 21,  1, 240, 0,  0, 0, 0,  0, 0, 0 }
     }
@@ -34,7 +32,21 @@ local function setPilotConfig(config)
     rf2.mspQueue:add(message)
 end
 
+local function getDefaults()
+    local defaults = {}
+    defaults.model_id = { min = 0, max = 99 }
+    local paramTypes = { [0] = "NONE", "TIMER1", "TIMER2", "TIMER3", "GV1", "GV2", "GV3", "GV4", "GV5", "GV6", "GV7", "GV8", "GV9" }
+    defaults.model_param1_type = { min = 0, max = #paramTypes, table = paramTypes }
+    defaults.model_param1_value = { min = -32000, max = 32000 }
+    defaults.model_param2_type = { min = 0, max = #paramTypes, table = paramTypes }
+    defaults.model_param2_value = { min = -32000, max = 32000 }
+    defaults.model_param3_type = { min = 0, max = #paramTypes, table = paramTypes }
+    defaults.model_param3_value = { min = -32000, max = 32000 }
+    return defaults
+end
+
 return {
-    getPilotConfig = getPilotConfig,
-    setPilotConfig = setPilotConfig
+    read = getPilotConfig,
+    write = setPilotConfig,
+    getDefaults = getDefaults
 }
--- src/SCRIPTS/RF2/PAGES/model.lua
+++ src/SCRIPTS/RF2/PAGES/model.lua
@@ -1,6 +1,6 @@
 local template = assert(rf2.loadScript(rf2.radio.template))()
-local mspPilotConfig = assert(rf2.loadScript("MSP/mspPilotConfig.lua"))()
-local mspName = assert(rf2.loadScript("MSP/mspName.lua"))()
+local mspPilotConfig = rf2.useApi("mspPilotConfig")
+local mspName = rf2.useApi("mspName")
 local settingsHelper = assert(rf2.loadScript("PAGES/helpers/settingsHelper.lua"))()
 local margin = template.margin
 local indent = template.indent
@@ -13,7 +13,7 @@ local y = yMinLim - lineSpacing
 local inc = { x = function(val) x = x + val return x end, y = function(val) y = y + val return y end }
 local labels = {}
 local fields = {}
-local pilotConfig = {}
+local pilotConfig = mspPilotConfig.getDefaults()
 local settings = settingsHelper.loadSettings()
 
 x = margin
@@ -22,39 +22,24 @@ y = yMinLim - tableSpacing.header
 labels[1] = { t = "---",                  x = x, y = inc.y(lineSpacing) }
 
 inc.y(lineSpacing * 0.25)
-fields[1] = { t = "Set name on TX",       x = x, y = inc.y(lineSpacing), sp = x + sp }
+fields[1] = { t = "Set name on TX",       x = x, y = inc.y(lineSpacing), sp = x + sp, data = { value = settings.autoSetName or 0, min = 0, max = 1, table = { [0] = "Off", "On" } } }
 labels[2] = { t = "Note: requires rf2bg", x = x + indent, y = inc.y(lineSpacing) }
 
 inc.y(lineSpacing * 0.25)
-fields[2] = { t = "Model ID",             x = x, y = inc.y(lineSpacing), sp = x + sp }
-fields[3] = { t = "Param1 type",          x = x, y = inc.y(lineSpacing), sp = x + sp }
-fields[4] = { t = "Param1 value",         x = x, y = inc.y(lineSpacing), sp = x + sp }
-fields[5] = { t = "Param2 type",          x = x, y = inc.y(lineSpacing), sp = x + sp }
-fields[6] = { t = "Param2 value",         x = x, y = inc.y(lineSpacing), sp = x + sp }
-fields[7] = { t = "Param3 type",          x = x, y = inc.y(lineSpacing), sp = x + sp }
-fields[8] = { t = "Param3 value",         x = x, y = inc.y(lineSpacing), sp = x + sp }
-
-local function setValues()
-    rf2.print("Auto set name" .. (settings.autoSetName or 0))
-    fields[1].data = { value = settings.autoSetName or 0, min = 0, max = 1, table = { [0] = "Off", "On" } }
-    fields[2].data = pilotConfig.model_id
-    fields[3].data = pilotConfig.model_param1_type
-    fields[4].data = pilotConfig.model_param1_value
-    fields[5].data = pilotConfig.model_param2_type
-    fields[6].data = pilotConfig.model_param2_value
-    fields[7].data = pilotConfig.model_param3_type
-    fields[8].data = pilotConfig.model_param3_value
-end
+fields[#fields + 1] = { t = "Model ID",             x = x, y = inc.y(lineSpacing), sp = x + sp, data = pilotConfig.model_id }
+fields[#fields + 1] = { t = "Param1 type",          x = x, y = inc.y(lineSpacing), sp = x + sp, data = pilotConfig.model_param1_type }
+fields[#fields + 1] = { t = "Param1 value",         x = x, y = inc.y(lineSpacing), sp = x + sp, data = pilotConfig.model_param1_value }
+fields[#fields + 1] = { t = "Param2 type",          x = x, y = inc.y(lineSpacing), sp = x + sp, data = pilotConfig.model_param2_type }
+fields[#fields + 1] = { t = "Param2 value",         x = x, y = inc.y(lineSpacing), sp = x + sp, data = pilotConfig.model_param2_value }
+fields[#fields + 1] = { t = "Param3 type",          x = x, y = inc.y(lineSpacing), sp = x + sp, data = pilotConfig.model_param3_type }
+fields[#fields + 1] = { t = "Param3 value",         x = x, y = inc.y(lineSpacing), sp = x + sp, data = pilotConfig.model_param3_value }
 
 local function onReceivedModelName(page, name)
     labels[1].t = name
     mspName = nil
 end
 
-local function onReceivedPilotConfig(page, config)
-    pilotConfig = config
-    setValues()
-    collectgarbage()
+local function onReceivedPilotConfig(page)
     rf2.lcdNeedsInvalidate = true
     page.isReady = true
 end
@@ -67,10 +52,10 @@ end
 return {
     read = function(self)
         mspName.getModelName(onReceivedModelName, self)
-        mspPilotConfig.getPilotConfig(onReceivedPilotConfig, self)
+        mspPilotConfig.read(pilotConfig, onReceivedPilotConfig, self)
     end,
     write = function(self)
-        mspPilotConfig.setPilotConfig(pilotConfig)
+        mspPilotConfig.write(pilotConfig)
         settings.autoSetName = fields[1].data.value
         settingsHelper.saveSettings(settings)
         pilotConfigReset()
