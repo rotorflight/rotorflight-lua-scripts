8bc848097caef2c916b1e2aee670b134b13718dc
Tao <gongtao0607@gmail.com>
2025-03-23 03:46:13 +0000

    Battery Profile: add lua support

--- src/SCRIPTS/RF2/pages.lua
+++ src/SCRIPTS/RF2/pages.lua
@@ -8,6 +8,7 @@ PageFiles[#PageFiles + 1] = { title = "Profile - PIDs", script = "pids.lua" }
 PageFiles[#PageFiles + 1] = { title = "Profile - Various", script = "profile.lua" }
 PageFiles[#PageFiles + 1] = { title = "Profile - Rescue", script = "profile_rescue.lua" }
 PageFiles[#PageFiles + 1] = { title = "Profile - Governor", script = "profile_governor.lua" }
+PageFiles[#PageFiles + 1] = { title = "Battery", script = "battery.lua" }
 PageFiles[#PageFiles + 1] = { title = "Servos", script = "servos.lua" }
 PageFiles[#PageFiles + 1] = { title = "Mixer", script = "mixer.lua" }
 PageFiles[#PageFiles + 1] = { title = "Gyro Filters", script = "filters.lua" }
