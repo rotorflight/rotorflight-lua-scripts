3584614e29f286023447026c361177ce5dcb171f
egonl <egon.lubbers@gmail.com>
2025-03-29 23:45:53 +0100

    chore: corrected spelling

--- src/SCRIPTS/RF2/PAGES/esc_flyrotor.lua
+++ src/SCRIPTS/RF2/PAGES/esc_flyrotor.lua
@@ -20,9 +20,9 @@ labels[#labels + 1] = { t = "---",                       x = x + indent, y = inc
 
 -- Basic
 labels[#labels + 1] = { t = "Basic",                     x = x,          y = incY(lineSpacing) }
-fields[#fields + 1] = { t = "ESC Mode",                  x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.esc_mode }
-fields[#fields + 1] = { t = "Cell Count [S]",            x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.cell_count }
-fields[#fields + 1] = { t = "BEC Voltage",               x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.bec_voltage }
+fields[#fields + 1] = { t = "ESC mode",                  x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.esc_mode }
+fields[#fields + 1] = { t = "Cell count [S]",            x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.cell_count }
+fields[#fields + 1] = { t = "BEC voltage",               x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.bec_voltage }
 fields[#fields + 1] = { t = "Motor direction",           x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.motor_direction }
 fields[#fields + 1] = { t = "Soft start",                x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.soft_start }
 fields[#fields + 1] = { t = "Fan control",               x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.fan_control }
@@ -38,20 +38,20 @@ fields[#fields + 1] = { t = "Buzzer volume",             x = x + indent, y = inc
 fields[#fields + 1] = { t = "Current gain",              x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.current_gain }
 
 -- Esc Governor
-labels[#labels + 1] = { t = "Esc Governor",              x = x,          y = incY(lineSpacing) }
-fields[#fields + 1] = { t = "Gov P-Gain",                x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.p_gain }
-fields[#fields + 1] = { t = "Gov I-Gain",                x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.i_gain }
-fields[#fields + 1] = { t = "Gov D-Gain",                x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.d_gain }
-fields[#fields + 1] = { t = "Motor ERPM Max",            x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.max_motor_erpm }
+labels[#labels + 1] = { t = "Esc governor",              x = x,          y = incY(lineSpacing) }
+fields[#fields + 1] = { t = "Gov P-gain",                x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.p_gain }
+fields[#fields + 1] = { t = "Gov I-gain",                x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.i_gain }
+fields[#fields + 1] = { t = "Gov D-gain",                x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.d_gain }
+fields[#fields + 1] = { t = "Motor max ERPM",            x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.max_motor_erpm }
 
 -- Other
 labels[#labels + 1] = { t = "Other",                     x = x, y = incY(lineSpacing) }
-fields[#fields + 1] = { t = "Throttle Protocol",         x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.throttle_protocol }
-fields[#fields + 1] = { t = "Tele Protocol",             x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.telemetry_protocol }
+fields[#fields + 1] = { t = "Throttle protocol",         x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.throttle_protocol }
+fields[#fields + 1] = { t = "Tele protocol",             x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.telemetry_protocol }
 fields[#fields + 1] = { t = "LED color",                 x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.led_color }
 fields[#fields + 1] = { t = "Motor temp sensor",         x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.motor_temp_sensor }
 fields[#fields + 1] = { t = "Motor temperture",          x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.motor_temp }
-fields[#fields + 1] = { t = "Capacity Cut-off",          x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.capacity_cutoff }
+fields[#fields + 1] = { t = "Capacity cutoff",           x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.capacity_cutoff }
 
 
 local function receivedEscParameters(page, data)
