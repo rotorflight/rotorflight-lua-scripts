df503d1d3005a660d49989dc901bd6b1644e1d0d
egonl <34315684+egonl@users.noreply.github.com>
2025-08-03 23:10:16 +0200

    feat: rf2tlm refactor (#114)
    Features:
    - Custom CRSF telemetry sensors can now be deleted and discovered at any time.
    - No need to follow a special procedure anymore to get the sensors in the correct order.
    - Reduced memory usage.

--- src/SCRIPTS/RF2/background.lua
+++ src/SCRIPTS/RF2/background.lua
@@ -5,17 +5,17 @@ local isInitialized = false
 local modelIsConnected = false
 local lastTimeRssi = nil
 
-local function pilotConfigReset()
-    model.setGlobalVariable(7, 8, 0)
-end
-
 local function pilotConfigHasBeenReset()
     return model.getGlobalVariable(7, 8) == 0
 end
 
+local hasSensor = rf2.executeScript("F/hasSensor")
+
 local function run()
     if rf2.runningInSimulator then
         modelIsConnected = true
+    elseif isInitialized and customTelemetryTask and not hasSensor("*Cnt") then
+        isInitialized = false -- user probably deleted all sensors on TX
     elseif getRSSI() > 0 then
         lastTimeRssi = rf2.clock()
         modelIsConnected = true
@@ -29,7 +29,7 @@ local function run()
             -- This is also a work-around for https://github.com/ExpressLRS/ExpressLRS/issues/3207 (AUX channel bug in ELRS TX < 3.5.5)
             return
         end
-        pilotConfigReset()
+        rf2.executeScript("F/pilotConfigReset")()
         if modelIsConnected then
             if initTask then
                 initTask.reset()
@@ -44,6 +44,9 @@ local function run()
     end
 
     if not isInitialized then
+        adjTellerTask = nil
+        customTelemetryTask = nil
+        collectgarbage()
         initTask = initTask or rf2.executeScript("background_init")
         local initTaskResult = initTask.run(modelIsConnected)
         if not initTaskResult.isInitialized then
@@ -51,13 +54,13 @@ local function run()
             return
         end
         if initTaskResult.crsfCustomTelemetryEnabled then
-            customTelemetryTask = rf2.executeScript("rf2tlm")
+            local requestedSensorsBySid = rf2.executeScript("rf2tlm_sensors", initTaskResult.crsfCustomTelemetrySensors)
+            customTelemetryTask = rf2.executeScript("rf2tlm", requestedSensorsBySid)
         end
         if initTask.useAdjustmentTeller then
             adjTellerTask = rf2.executeScript("adj_teller")
         end
         initTask = nil
-        collectgarbage()
         isInitialized = true
     end
 
@@ -68,7 +71,6 @@ local function run()
     if adjTellerTask and adjTellerTask.run() == 2  then
         -- no adjustment sensors found
         adjTellerTask = nil
-        collectgarbage()
     end
 
     if customTelemetryTask then
@@ -78,7 +80,11 @@ end
 
 local function runProtected()
     local status, err = pcall(run)
-    --if not status then rf2.print(err) end
+    --[NIR
+    if not status then rf2.print(err) end
+    --]]
+    --collectgarbage()
+    --rf2.print("Mem: %d", collectgarbage("count")*1024)
     return 0
 end
 
