4416db58f84dd3790409cf399e147f130c09b97f
Stan Hung <drung0418@gmail.com>
2025-10-18 11:14:11 +0800

    feat: add 320x240 support

--- src/SCRIPTS/RF2/radios.lua
+++ src/SCRIPTS/RF2/radios.lua
@@ -75,6 +75,19 @@ local supportedRadios =
             yMaxLimit = 435,
         },
     },
+    ["320x240"] =
+    {
+        msp = {
+            template = "TEMPLATES/320x240.lua",
+            highRes = true,
+            MenuBox = { x= (LCD_W -200)/2, y=LCD_H/2, w=200, x_offset=68, h_line=20, h_offset=6 },
+            SaveBox = { x= (LCD_W -200)/2, y=LCD_H/2, w=180, x_offset=12, h=60, h_offset=12 },
+            NoTelem = { LCD_W/2 - 50, LCD_H - 28, "No Telemetry", (TEXT_COLOR or 0) + INVERS + BLINK },
+            textSize = 0,
+            yMinLimit = 35,
+            yMaxLimit = 200,
+        },
+    },
 }
 
 local resolution = LCD_W.."x"..LCD_H
