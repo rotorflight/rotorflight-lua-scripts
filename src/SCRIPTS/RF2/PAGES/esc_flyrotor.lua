7938fec57df1772f96a0de700865b53f568aa69e
MO <52574424+Jiki-Mo@users.noreply.github.com>
2026-01-11 00:22:44 +0800

    chore: updated FLYROTOR ESC page (#120)
    * Modify FLYROTOR ESC parameter range
    * Change the gov_d project to drive_freq to support users to modify the drive frequency of the motor.
    * Added new setting items and re-formatted.
    * Added new automatic restart time and restart acceleration settings; the layout was reformatted to be consistent with the PC configuration software.
    * Change the description character in the FLYROTOR ESC settings.
    * FLYROTOR ESC settings synchronize the description characters with the PC software to ensure consistent descriptions.
    * Add active freewheeling setting

--- src/SCRIPTS/RF2/PAGES/esc_flyrotor.lua
+++ src/SCRIPTS/RF2/PAGES/esc_flyrotor.lua
@@ -19,45 +19,46 @@ labels[#labels + 1] = { t = "---",                       x = x + indent, y = inc
 labels[#labels + 1] = { t = "---",                       x = x + indent, y = incY(lineSpacing), bold = false }
 labels[#labels + 1] = { t = "---",                       x = x + indent, y = incY(lineSpacing), bold = false }
 
--- Basic
-labels[#labels + 1] = { t = "Basic",                     x = x,          y = incY(lineSpacing) }
-fields[#fields + 1] = { t = "ESC mode",                  x = x + indent, y = incY(lineSpacing), sp = x + sp, w = 125, data = escParameters.esc_mode }
-fields[#fields + 1] = { t = "Cell count [S]",            x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.cell_count }
-fields[#fields + 1] = { t = "Low voltage",               x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.low_voltage }
-fields[#fields + 1] = { t = "ESC Temperature",           x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.temperature }
-fields[#fields + 1] = { t = "SBEC voltage",              x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.bec_voltage }
-fields[#fields + 1] = { t = "Electrical angle",          x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.timing }
-fields[#fields + 1] = { t = "Motor direction",           x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.motor_direction }
-fields[#fields + 1] = { t = "Starting torque",           x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.starting_torque }
-fields[#fields + 1] = { t = "Response speed",            x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.response_speed }
-fields[#fields + 1] = { t = "Buzzer volume",             x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.buzzer_volume }
-fields[#fields + 1] = { t = "Current gain",              x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.current_gain }
-fields[#fields + 1] = { t = "Fan control",               x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.fan_control }
+-- General
+labels[#labels + 1] = { t = "General Parameters",        x = x,          y = incY(lineSpacing) }
+fields[#fields + 1] = { t = "ESC Mode",                  x = x + indent, y = incY(lineSpacing), sp = x + sp, w = 125, data = escParameters.esc_mode }
+fields[#fields + 1] = { t = "LiPo Cell Count [S]",       x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.cell_count }
+fields[#fields + 1] = { t = "Low Voltage Limit",         x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.low_voltage }
+fields[#fields + 1] = { t = "Temperature Limit",         x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.temperature }
+fields[#fields + 1] = { t = "SBEC Voltage",              x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.bec_voltage }
+fields[#fields + 1] = { t = "Electrical Angle",          x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.timing }
+fields[#fields + 1] = { t = "Motor Direction",           x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.motor_direction }
+fields[#fields + 1] = { t = "Starting Power",            x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.starting_torque }
+fields[#fields + 1] = { t = "Response Speed",            x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.response_speed }
+fields[#fields + 1] = { t = "Beeper Volume",             x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.buzzer_volume }
+fields[#fields + 1] = { t = "Current Gain",              x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.current_gain }
+fields[#fields + 1] = { t = "Cooling Fan Mode",          x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.fan_control }
 
 -- Advanced
-labels[#labels + 1] = { t = "Advanced",                  x = x,          y = incY(lineSpacing) }
-fields[#fields + 1] = { t = "Soft start time",           x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.soft_start }
-fields[#fields + 1] = { t = "Auto restart time",         x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.auto_restart_time }
-fields[#fields + 1] = { t = "Restart Acc",               x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.restart_acc }
-fields[#fields + 1] = { t = "Gov P-gain",                x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.p_gain }
-fields[#fields + 1] = { t = "Gov I-gain",                x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.i_gain }
-fields[#fields + 1] = { t = "Drive frequency [KHz]",     x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.drive_freq }
-fields[#fields + 1] = { t = "Motor max ERPM",            x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.max_motor_erpm }
+labels[#labels + 1] = { t = "Advanced Parameters",       x = x,          y = incY(lineSpacing) }
+fields[#fields + 1] = { t = "Soft Start Time",           x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.soft_start }
+fields[#fields + 1] = { t = "Auto Bailout Time",         x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.auto_restart_time }
+fields[#fields + 1] = { t = "Auto Bailout Accel",        x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.restart_acc }
+fields[#fields + 1] = { t = "Governor P",                x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.p_gain }
+fields[#fields + 1] = { t = "Governor I",                x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.i_gain }
+fields[#fields + 1] = { t = "Active Freewheeling",       x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.active_freewheel }
+fields[#fields + 1] = { t = "Drive Frequency [KHz]",     x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.drive_freq }
+fields[#fields + 1] = { t = "Maximun Motor ERPM",        x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.max_motor_erpm }
 
 -- Other
-labels[#labels + 1] = { t = "Other",                     x = x, y = incY(lineSpacing) }
-fields[#fields + 1] = { t = "Throttle protocol",         x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.throttle_protocol }
-fields[#fields + 1] = { t = "Tele protocol",             x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.telemetry_protocol }
-fields[#fields + 1] = { t = "LED color",                 x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.led_color }
-fields[#fields + 1] = { t = "Motor temp sensor",         x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.motor_temp_sensor }
-fields[#fields + 1] = { t = "Motor temperture",          x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.motor_temp }
-fields[#fields + 1] = { t = "Capacity cutoff",           x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.capacity_cutoff }
+labels[#labels + 1] = { t = "Other Parameters",          x = x, y = incY(lineSpacing) }
+fields[#fields + 1] = { t = "Throttle Protocol",         x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.throttle_protocol }
+fields[#fields + 1] = { t = "Telemetry Protocol",        x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.telemetry_protocol }
+fields[#fields + 1] = { t = "LED Color",                 x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.led_color }
+fields[#fields + 1] = { t = "Motor Temperture Sensor",   x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.motor_temp_sensor }
+fields[#fields + 1] = { t = "Motor Temperture Limit",    x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.motor_temp }
+fields[#fields + 1] = { t = "Capacity Limit [mAh]",      x = x + indent, y = incY(lineSpacing), sp = x + sp, data = escParameters.capacity_cutoff }
 
 local function receivedEscParameters(page, data)
     if data.esc_signature ~= 115 then -- Flyrotor signature
         page.labels[1].t = "Invalid ESC detected"
     else
-        page.labels[1].t = string.format("FLYROTOR %dA", data.amperage)
+        page.labels[1].t = string.format("FLYROTOR %dA%s", data.amperage, data.type == 1 and " F3C" or "")
         page.labels[2].t = string.format("S/N: %08X%08X", data.serial_number1, data.serial_number2)
         page.labels[3].t = string.format("HW: 1.%d - IAP: %d.%d.%d", data.hw_version, data.iap_major, data.iap_minor, data.iap_patch)
         page.labels[4].t = string.format("FW: %d.%d.%d", data.fw_major, data.fw_minor, data.fw_patch)
