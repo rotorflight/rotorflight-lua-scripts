32af22ea1fecb204776e230575e62dba3d80e963
egonl <egon.lubbers@gmail.com>
2026-03-02 20:28:10 +0100

    rf2bg: reduced resource usage when not active

--- src/SCRIPTS/FUNCTIONS/rf2bg.lua
+++ src/SCRIPTS/FUNCTIONS/rf2bg.lua
@@ -1,7 +1,17 @@
-assert(loadScript("/SCRIPTS/RF2/rf2.lua"))()
-rf2.mspQueue = rf2.executeScript("MSP/mspQueue")
-rf2.mspQueue.maxRetries = 3
-rf2.mspHelper = rf2.executeScript("MSP/mspHelper")
-local background = rf2.executeScript("background")
+local initialized = false
+local backgroundTask = nil
 
-return { run = background }
+local function startup()
+    if not initialized then
+        assert(loadScript("/SCRIPTS/RF2/rf2.lua"))()
+        rf2.mspQueue = rf2.executeScript("MSP/mspQueue")
+        rf2.mspQueue.maxRetries = 3
+        rf2.mspHelper = rf2.executeScript("MSP/mspHelper")
+        backgroundTask = rf2.executeScript("background")
+        initialized = true
+    else
+        backgroundTask()
+    end
+end
+
+return { run = startup }
