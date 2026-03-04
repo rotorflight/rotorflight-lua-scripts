e220819a993b591682c36331b9f6a4f154529d0c
egonl <egon.lubbers@gmail.com>
2026-03-04 23:20:19 +0100

    Improved comments

--- src/WIDGETS/RfStats/app.lua
+++ src/WIDGETS/RfStats/app.lua
@@ -1,4 +1,8 @@
--- RfStats widget
+-- This RfStats widget demonstrates how to:
+-- * register a widget with RfTool using rf2.registerWidget()
+-- * receive model state changes using onStateChanged()
+-- * use MSP to retrieve data from the flight controller using rf2.useApi()
+
 local zone, options = ...
 
 local w = {
@@ -38,9 +42,9 @@ end
 
 w.background = function(widget)
     -- Called when the widget isn't visible.
-    if rf2 and not widget.registered then
+    if rf2 and not widget.isRegistered then
         rf2.registerWidget(widget)
-        widget.registered = true
+        widget.isRegistered = true
     end
 end
 
@@ -50,17 +54,19 @@ w.refresh = function(widget, event, touchState)
 end
 
 local function onReceivedFlightStats(callbackParam, stats)
+    -- See SCRIPTS/RF2/MSP/mspFlightStats.lua for all keys in stats
     totalFlights = tostring(stats.stats_total_flights.value)
     totalFlightTime = rf2.executeScript("F/formatSeconds")(stats.stats_total_time_s.value)
 end
 
 w.onStateChanged = function(widget, newState)
-    -- Called by RfTool if the widget is registered and the model state changes.
+    -- Called by RfTool when the widget is registered and the model state changes.
     --   newState can be: "connected", "disconnected", "armed" or "disarmed".
     -- Note:  RfTool requires the 'ARM' sensor for setting "armed" and "disarmed".
 
+    --rf2.print("newState: %s", newState)
     if newState == "connected" or newState == "disarmed" then
-        rf2.useApi("mspFlightStats").read(onReceivedFlightStats)
+        rf2.useApi("mspFlightStats").read(onReceivedFlightStats, "unused example callback parameter")
     elseif newState == "disconnected" then
         totalFlights = nil
         totalFlightTime = nil
