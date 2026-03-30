0e7e6a256dbc5f6a50bb71254b20af138e2f3f1f
egonl <egon.lubbers@gmail.com>
2026-03-30 23:41:55 +0200

    chore: bumped version

--- src/SCRIPTS/RF2/rf2.lua
+++ src/SCRIPTS/RF2/rf2.lua
@@ -1,5 +1,5 @@
 rf2 = {
-    luaVersion = "2.3.0-20260305",
+    luaVersion = "2.3.0-RC1",
     baseDir = "/SCRIPTS/RF2/",
     runningInSimulator = string.sub(select(2, getVersion()), -4) == "simu",
 
